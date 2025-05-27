22c818cbcff84e1f29819afeddaa031a3725f504
Felipe Jovino <Felipejovinogamerplay@gmail.com>
2025-05-27 17:15:36 -0300

    feat: models e relacionamentos

--- app/models/user.ts
+++ app/models/user.ts
@@ -1,9 +1,12 @@
 import { DateTime } from 'luxon'
 import hash from '@adonisjs/core/services/hash'
 import { compose } from '@adonisjs/core/helpers'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { BaseModel, column, hasMany } from '@adonisjs/lucid/orm'
 import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
 import { DbAccessTokensProvider } from '@adonisjs/auth/access_tokens'
+import { type HasMany } from '@adonisjs/lucid/types/relations'
+
+import Task from './task.js'
 
 const AuthFinder = withAuthFinder(() => hash.use('scrypt'), {
   uids: ['email'],
@@ -15,13 +18,16 @@ export default class User extends compose(BaseModel, AuthFinder) {
   declare id: number
 
   @column()
-  declare fullName: string | null
+  declare name: string
 
   @column()
   declare email: string
 
   @column({ serializeAs: null })
   declare password: string
+
+  @column()
+  declare token: string | null
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
@@ -30,4 +36,7 @@ export default class User extends compose(BaseModel, AuthFinder) {
   declare updatedAt: DateTime | null
 
   static accessTokens = DbAccessTokensProvider.forModel(User)
+
+  @hasMany(()=> Task)
+  declare tasks: HasMany<typeof Task>
 }
