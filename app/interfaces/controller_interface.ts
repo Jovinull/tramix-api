02e64697a494f79c8302b1a3bf8c1e758ecc61eb
Felipe Jovino <Felipejovinogamerplay@gmail.com>
2025-05-28 17:54:25 -0300

    feat: implementação de interfaces

---
+++ app/interfaces/controller_interface.ts
@@ -0,0 +1,9 @@
+import type { HttpContext } from '@adonisjs/core/http';
+
+export interface ControllerInterface {
+  index(ctx: HttpContext): Promise<any>;
+  show(ctx: HttpContext): Promise<any>;
+  store(ctx: HttpContext): Promise<any>;
+  update(ctx: HttpContext): Promise<any>;
+  destroy(ctx: HttpContext): Promise<any>;
+}
